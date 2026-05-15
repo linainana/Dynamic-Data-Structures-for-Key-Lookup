4fc7b509a593b78605ca213956c8e0d9e7ae5835
fm437352 <fm437352@gmail.com>
2026-05-14 22:49:06 -0400

    Update Funciones.cpp

--- src/solucion2/Funciones.cpp
+++ src/solucion2/Funciones.cpp
@@ -1,1 +1,24 @@
+#includ "Funciones.h"
 
+int length(uchar* s){
+  int i = 0;
+  while(s[i] != '\0')
+    i++;
+  return i;
+}
+
+int compare(uchar* a, uchar* b){
+  int i = 0;
+  while(a[i] != '\0' && b[i] != '\0'){
+    if(a[i] < b[i])
+      return -1;
+    if(a[i] > b[i])
+      return 1;
+    i++;
+  }
+  if(a[i] == '\0' && b[] == '\0')
+    return 0;
+  if(a[i] == '\0')
+    return -1;
+  return 1;
+}
