922da3a52ffc22e49843d3a1e4fa3c643158c30f
Escarlett <escarlett.vargas.a@gmail.com>
2026-05-14 22:35:55 -0400

    Add insert method to Grilla class

--- src/solucion2/Grilla.cpp
+++ src/solucion2/Grilla.cpp
@@ -10,7 +10,8 @@ Grilla::Grilla(int K){
 Grilla::~Grilla(){
   deleteLevels(head);
 }
-//nodo
+
+//nodos
 Nodo* Grilla::createNode(uchar* palabra){
   Nodo* nuevo = new Nodo;
 
@@ -21,4 +22,44 @@ Nodo* Grilla::createNode(uchar* palabra){
   nuevo->down = nullptr;
 
   return nuevo;
+}
+
+//insertar
+bool Grilla::insert(uchar* palabra){
+  Nodo* nuevo = createNode(palabra);
+  //lista vacía
+  if(head == nullptr){
+    head = nuevo;
+    return true;
+  }
+  //insertar al inicio
+  if(compare(palabra, head->key) < 0){
+    nuevo->next = head;
+    head->prev = nuevo;
+    head = nuevo;
+    return true;
+  }
+  Nodo* p = head;
+  while(p->next != nullptr &&
+  compare(p->next->key, palabra) < 0){
+    p = p->next;
+  }
+  //palabra repetida
+  if(compare(p->key, palabra) == 0){
+    delete[] nuevo->key;
+    delete nuevo;
+    return false;
+  }
+  if(p->next != nullptr &&
+    compare(p->next->key, palabra) == 0){
+      delete[] nuevo->key;
+      delete nuevo; 
+      return false;
+  }
+  nuevo->next = p->next;
+  if(p->next != nullptr)
+    p->next->prev = nuevo;
+  nuevo->prev = p;
+  p->next = nuevo;
+  return true;
 }
