38cf14160874d71e03355a40b705a9cee99362e4
fm437352 <fm437352@gmail.com>
2026-05-14 23:10:55 -0400

    Update Grilla.cpp

--- src/solucion2/Grilla.cpp
+++ src/solucion2/Grilla.cpp
@@ -77,3 +77,45 @@ Nodo* Grilla::search(uchar* palabra){
   }
   return nullptr;
 }
+
+//eliminar 
+
+bool Grilla::remove(uchar* palabra){
+//bajar hasta el nivel base (L1)
+  Nodo* base = head;
+  if(base == nullptr) return false;
+  while(base->down != nullptr) base = base->down;
+  //buscar la palbra en el nivel base (recorrido lineal)
+  Nodo* p = base;
+  while(p != nullptr && commpare(p->key, palabra) 1= 0){
+    p = p->next;
+  }
+  //si no se encuentra, no hay nada que eliminar 
+  if(p == nullptr) return false;
+ 
+  //desconectar el nodo de la lista doblemente enlazada (L1)
+  if(p->prev != nullptr){
+     p->prev->next = p->prev;
+  } else {
+     //si era el primero, el nuevo inicio de la base es el siguiente
+     base = p->next;
+  }
+
+  if(p->next != nullptr){
+     p->next->prev = p->prev;
+  }
+
+  //liberar la memoria del nodo eliminado en L1
+  delete[] p->key;
+  delete p;
+
+  //saneanmiento y actualización de la grilla 
+  //borramos los índices viejos que ya no son validos 
+  deleteUpperLevels();
+
+  //ahora 'head' apunta al inicio de L1
+  //reconsttruimos los niveles superiores para mantener la eficiencia de búsqueda
+  rebuindGrid();
+
+  return true;
+}
