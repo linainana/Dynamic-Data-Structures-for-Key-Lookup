cb6f9780f84d326aa07e89a62fc7d58add2ec569
fm437352 <fm437352@gmail.com>
2026-05-14 22:33:03 -0400

    Update Funciones.h

--- src/solucion2/Funciones.h
+++ src/solucion2/Funciones.h
@@ -9,3 +9,13 @@ using namespace std;
 
 typedef unsigned char uchar;
 
+//funnciones para uchar*
+int length(uchar* s);//largo de una palabra
+int compare(uchar* a, uchar* b); //compara lexicográficamente
+//retorna -1 si a<b, 0 si a==b, 1 si a>b
+uchar* copyString(uchar* s); //copia una palabra 
+
+//archivos
+void cleanLine(uchar* s); //elimina salto de línea
+uchar* readWord(ifstream& file); //lee una palabra desde archivo
+#endif
