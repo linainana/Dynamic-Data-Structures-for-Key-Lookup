60dca0802b7f93b9d115e18383a537cd1268f6ce
Escarlett <escarlett.vargas.a@gmail.com>
2026-05-14 22:19:34 -0400

    Update Grilla.h

--- src/solucion2/Grilla.h
+++ src/solucion2/Grilla.h
@@ -17,3 +17,34 @@ struct nodoGrilla{
    nodoGrilla* down; //mismo nodo en nivel inferior 
 };
 typedef struct nodoGrilla Nodo;
+
+class Grilla{
+public:
+   Nodo* head;
+   int levels;
+   int k;
+
+   //constructor y destructor
+   Grilla(int K);
+   virtual ~Grilla();
+
+   //operaciones principales
+   bool insert(uchar* palabra);
+   Nodo* search(uchar* palabra);
+   bool remove(uchar* palabra);
+
+   //impresión
+   void printLevel(Nodo* l);
+   void printGrid();
+
+   //manejo de nodos
+   Nodo* createNode(uchar* palabra);
+   void deleteLevels(Nodo* l);
+
+   //niveles
+   int sizeLevel(Nodo* l);
+   Nodo* buildUpperLevel(Nodo* lower);
+   void rebuildGrid();
+   void deleteUpperLevels(); 
+};
+#endif
